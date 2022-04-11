91df01e19799d99e28d1095c80165952a87b26c8
Y Tang <ytangw03854@gmail.com>
2022-04-11 02:29:48 -0500

    Finished

--- M6-Wheel-Game.py
+++ M6-Wheel-Game.py
@@ -8,6 +8,7 @@
 # reward for winning round 1 or 2 is 1000 * the round
 # turn continues from where it left off, ie player 1 solves round 1, round 2 starts with player 2
 # lose your turn if you buy a wrong vowel, otherwise 1 person with bank just buys out everything
+#see readme for other rules notes
 
 import random
 
@@ -35,7 +36,10 @@ def round1and2():
     global usedWords
     while tempWord in usedWords:
         tempWord = wordList[random.randint(0,len(wordList)-1)]
-    usedWords += tempWord
+    usedWords.append(tempWord)
+
+    # UNCOMMENT THIS NEXT LINE FOR TESTING
+    #print(usedWords)
     # 2 versions for a workaround later
     realWord = remainWord = tempWord
     answer = ""
@@ -43,8 +47,10 @@ def round1and2():
         answer += "_"
     letters = []
     # global to persist across games
+    # some might not have needed to be globals for this but w/e
     global gameRound, playTurn, playMoney, wheelOptions
 
+    #repeat until round ends
     runRound = True
     # game starts
     while (runRound):
@@ -68,15 +74,23 @@ def round1and2():
                 print("Must be an integer menu option")
 
         if menuItem == 1:
-            #mb add check if valid consonants to guess, could have all consonant but still missing 1 vowel
+            # check if valid consonants left to guess, could have all consonant but still missing 1 vowel
+            numVowels = (letters.count("a") + letters.count("e") + letters.count("i") + letters.count("o") +
+                letters.count("u") )
+            if len(letters) - numVowels >= 21:
+                print("No more consonants left to guess")
+                continue
+            #spin wheel
             spin = wheelOptions[random.randint(0,len(wheelOptions)-1)]
             if spin == 0:
                 print("You went bankrupt. Next player's turn")
                 playMoney[playTurn-1] = 0
                 playTurn += 1
+                continue
             elif spin == 1: 
                 print("You lost your turn. Next player's turn")
                 playTurn += 1
+                continue
             print(f"You spun ${spin}")
             guess = ""
             # nested while to get guesses until 1 is valid
@@ -98,6 +112,7 @@ def round1and2():
             if  remainWord.find(guess) == -1:
                 print("That letter was not found. Next player's turn")
                 playTurn += 1
+                continue
             # essentially "moves" letter from a duplicate of the true word to the answer so find can be used for repeated letters  
             # and doesn't only find the first match
             correctLetters = 0 # track how much money to give
@@ -115,11 +130,17 @@ def round1and2():
                 playMoney[playTurn-1] += gameRound * 1000
                 gameRound += 1
                 playTurn += 1
-                
+        #buy vowel
         elif menuItem == 2:
             if playMoney[playTurn-1] < 250:
                 print("You don't have the money to buy a vowel")
-            #copied form 1, guess meaning vowel
+                continue
+            # can't buy more vowels if none are left
+            if (letters.count("a") == 1 and letters.count("e") == 1 and letters.count("i") == 1 and letters.count("o") == 1
+                and letters.count("u") == 1 ):
+                print("There aren't any more vowels to buy")
+                continue
+            #copied from 1, guess meaning vowel
             guess = ""
             # nested while to get vowels until 1 is valid
             invalGuess = True
@@ -158,7 +179,7 @@ def round1and2():
                 playMoney[playTurn-1] += gameRound * 1000
                 gameRound += 1
                 playTurn += 1
-            
+        #guess word    
         elif menuItem == 3:
             guess = str(input("Try to guess the word: ")).lower()
             # thought about only allowing words of correct length but technically players can guess whatever they want
@@ -177,17 +198,130 @@ def round1and2():
                 playTurn += 1
 
 def round3():
-    print(playMoney)
+    #generate word
+    tempWord = wordList[random.randint(0,len(wordList)-1)]
+    global usedWords
+    while tempWord in usedWords:
+        tempWord = wordList[random.randint(0,len(wordList)-1)]
+    usedWords.append(tempWord)
+    # 2 versions for a workaround later
+    realWord = remainWord = tempWord
+    answer = ""
+    for i in range(len(remainWord)):
+        answer += "_"
+    letters = []
+    global gameRound
+    
+    #if tied, goes to player 1 or first player who's tied
+    playNum = 0
+    for i in range(3):
+        if playMoney[i] == max(playMoney):
+            print(f"----------------------\nRound 3: Player {i+1}, you had the most money with ${playMoney[i]}, so you will play")
+            print("""R,S,T,L,N and E have been revealed for this puzzle. guess 3 more consonants and 1 vowel, then you 
+will have to guess the final puzzle to win $5000""")
+            playNum = i+1
+            continue
+    
+    #preadd letters to answer
+    letters.append("r")
+    letters.append("s")
+    letters.append("t")
+    letters.append("l")
+    letters.append("n")
+    letters.append("e")
+    # definitely a better way to do this but this reuses what's alreayd written
+    guess = "r"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    guess = "s"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    guess = "t"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    guess = "l"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    guess = "n"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    guess = "e"
+    while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):]
+    
+    #get consonants and add them to answer
+    for i in range(3):
+        print(answer)
+        invalGuess = True
+        while (invalGuess):
+                guess = str(input("Enter a consonant: ")).lower()
+                if not guess.isalpha():
+                    print("Please enter a letter")
+                elif len(guess) != 1 :
+                    print("Please only guess 1 letter at a time")
+                elif guess in letters:
+                    print("That letter has already been guessed, try another one")
+                elif guess == "a" or guess == "e" or guess == "i" or guess == "o" or guess == "u":
+                    print("You cannot enter vowels at this time, guess a consonant")
+                else:
+                    letters += guess
+                    invalGuess = False
+        while remainWord.find(guess) != -1:
+                answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+                remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):] 
+
+    #get vowel 
+    invalGuess = True
+    while (invalGuess):
+                print(answer)
+                guess = str(input("Enter a vowel: ")).lower()
+                if not guess.isalpha():
+                    print("Please enter a letter")
+                elif len(guess) != 1 :
+                    print("Please only choose 1 vowel at a time")
+                elif guess in letters:
+                    print("That letter has already been guessed, try another one")
+                # changed ors to ands and == to != but really should have just made this the correct condition lol
+                elif guess != "a" and guess != "e" and guess != "i" and guess != "o" and guess != "u":
+                    print("Please select a vowel")
+                else:
+                    letters += guess
+                    invalGuess = False
+
+            # essentially "moves" letter from a duplicate of the true word to the answer so find can be used for repeated letters  
+            # and doesn't only find the first match
+    while remainWord.find(guess) != -1:
+            answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+len(guess):]
+            remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+len(guess):] 
+
+    print(answer)
+    guess = str(input("Try to guess the word: ")).lower()
+            # thought about only allowing words of correct length but technically players can guess whatever they want
+    while not guess.isalpha():
+                print("Error. Illegal characters or numbers found. Try again")
+                guess = str(input("Try to guess the word: ")).lower()
+    if guess.lower() == realWord:
+                print(f"That's correct. The word was {realWord}. You get $5000, Player {playNum}, making your total ${5000 + playMoney[playNum-1]}!")
+                gameRound += 1
+    else:
+                print(f"That's too bad, The word was {realWord}. You lost, but you still got ${playMoney[playNum-1]}!")
+                gameRound += 1
+
 
 print("Welcome to Wheel of Fortune! Please see the Readme for notes on this variation of the game")
 
-# play game again if desired
+# play game rounds
 while(gameRound==1 or gameRound==2):
     round1and2()
 
 while(gameRound==3):
     round3()
-    print("round 3")
-    break
+print("Game over")
 
 f.close()
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # M6-Wheel
+
+Simulation of Wheel of fortune with some alternate rules for 3 players(can be played by 1)
+
+rules notes: players can buy vowels whenever during their turn, ie don't need to spin first
+players spin for each consonant guess, and CAN spin the same value
+only 19 instead of 24 wheel options, bankrupt, lose turn and 17 values 100 to 900 by 50, no duplicates
+since the wheel is less harsh, there's no money that's immune. ie, you can use money from past round to buy vowels, but
+bankrupts will make the player lose EVERYTHING
+reward for winning round 1 or 2 is 1000 * the round
+turn continues from where it left off, ie player 1 solves round 1, round 2 starts with player 2
+lose your turn if you buy a wrong vowel, otherwise 1 person with bank just buys out everything
+goes by player order for who plays round 3 if multiple people are tied
+no time limit for final guess because that's a bit anticlimactic to lose to a clock
